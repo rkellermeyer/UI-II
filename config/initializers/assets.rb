10472ca582210b8be48ca6dbd80102651351b2f3
Richard Kellermeyer <rnkconcepts@gmail.com>
2016-09-16 00:54:46 -0400

    Add video player samples

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,47 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w(counterup.min.js
+                                                custom.js
+                                                flexibility.js
+                                                headroom.min.js
+                                                jquery.contact-form.js
+                                                jquery.countdown.min.js
+                                                jquery.event.move.js
+                                                jquery.flexslider-min.js
+                                                jquery.gmaps.min.js
+                                                jquery.interactive_bg.js
+                                                jquery.isotope.min.js
+                                                jquery.jpanelmenu.js
+                                                jquery.magnific-popup.min.js
+                                                jquery.photogrid.js
+                                                jquery.pricefilter.js
+                                                jquery.royalslider.min.js
+                                                jquery.stacktable.js
+                                                jquery.sticky-kit.min.js
+                                                jquery.themepunch.revolution.min.js
+                                                jquery.themepunch.tools.min.js
+                                                jquery.tooltips.min.js
+                                                jquery.twentytwenty.js
+                                                modernizr.custom.js
+                                                owl.carousel.min.js
+                                                puregrid.js
+                                                switcher.js
+                                                view.js
+                                                waypoints.min.js
+                                                extensions/revolution.extension.actions.min.js
+                                                extensions/revolution.extension.carousel.min.js
+                                                extensions/revolution.extension.kenburn.min.js
+                                                extensions/revolution.extension.layeranimation.min.js
+                                                extensions/revolution.extension.migration.min.js
+                                                extensions/revolution.extension.navigation.min.js
+                                                extensions/revolution.extension.parallax.min.js
+                                                extensions/revolution.extension.slideanims.min.js
+                                                extensions/revolution.extension.video.min copy.js
+                                                extensions/revolution.extension.video.min.js
+                                                bootstrap.css
+                                                icons.css
+                                                revolutionslider.css
+                                                style.css
+                                                colors/blue.css)
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
   devise_for :members
   root to: 'pages#home'
 end
